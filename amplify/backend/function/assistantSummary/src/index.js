60a22c6178bbe594fb991c8f9e3a6c945f98b607
Karla Gardiner <karla.gardiner@digital.cabinet-office.gov.uk>
2024-06-24 21:41:26 +0100

    40: updated lambda to invoke bedrock and return summary

--- amplify/backend/function/assistantSummary/src/index.js
+++ amplify/backend/function/assistantSummary/src/index.js
@@ -1,17 +1,40 @@
+import { BedrockRuntimeClient, InvokeModelCommand } from '@aws-sdk/client-bedrock-runtime';
 
-
-/**
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- */
-exports.handler = async (event) => {
+export const handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
+
+    const client = new BedrockRuntimeClient({ region: 'eu-west-2'});
+
+    const input = {
+        modelId: "amazon.titan-text-express-v1",
+        contentType: "application/json",
+        accept: "*/*",
+        body: JSON.stringify({
+          inputText: `Write a summary that's no longer than 1000 characters of: ${event.arguments.summary}. Make it interest someone into reading the full text.`,
+          textGenerationConfig: {
+              maxTokenCount: 8192,
+              stopSequences: [],
+              temperature: 0.7,
+              topP: 1,
+          }
+        })
+      }
+
+      let text = '';
+      try {
+        const data = await client.send(new InvokeModelCommand(input));
+        const jsonString = Buffer.from(data.body).toString('utf-8');
+        const parsedData = JSON.parse(jsonString);
+        text = parsedData.results[0].outputText;
+
+      } catch(error) {
+        console.log({ error });
+      }
+
+
+
     return {
         statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-        body: JSON.stringify('Hello from Lambda!'),
+        body: text,
     };
 };
