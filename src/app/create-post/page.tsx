5615a688ca609758dcf7d4bb1d0bf293c89f89d8
Karla Gardiner <karla.gardiner@digital.cabinet-office.gov.uk>
2024-06-24 15:34:15 +0100

    37: added summary field to create page

--- src/app/create-post/page.tsx
+++ src/app/create-post/page.tsx
@@ -130,6 +130,15 @@ function CreatePost() {
         isboldFont={true}
         onChange={onChange}
       />
+      <label className="block my-4">
+        <span className="text-sm font-normal">Summary</span>
+        <textarea
+          className="border-b pb-2 text-l focus:outline-none w-full text-gray-500 placeholder:text-gray-500 placeholder:font-normal placeholder:text-base y-2"
+          placeholder="Enter a blog summary"
+          name="summary"
+          onChange={onChange}
+        />
+      </label>
       {image &&
         <Image
           src={URL.createObjectURL(image)}
