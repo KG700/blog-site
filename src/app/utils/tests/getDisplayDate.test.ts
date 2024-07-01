d52c252b1da9b609acc5dbf1d936cddbc86be3f2
Karla Gardiner <karla.gardiner@digital.cabinet-office.gov.uk>
2024-07-01 16:24:37 +0100

    4: added proper passing unit test

--- src/app/utils/tests/getDisplayDate.test.ts
+++ src/app/utils/tests/getDisplayDate.test.ts
@@ -1,7 +1,18 @@
 import { getDisplayDate } from '../getDisplayDate';
 
 describe('getDisplayDate', () => {
-    it('returns date with timestamp when passed isoDate and timeStamp true', () => {
-        expect('Hello').toEqual('Hello')
+    beforeEach(() => {
+        jest.useFakeTimers();
+        jest.setSystemTime(new Date(Date.UTC(2024, 6, 15)));
+      });
+
+    afterAll(() => {
+        jest.useRealTimers();
+    });
+
+    it('returns today when passed todays isoDate', () => {
+        const todaysDate = new Date()
+        const theDate = getDisplayDate(todaysDate.toISOString())
+        expect(theDate).toEqual('today')
     })
 })
