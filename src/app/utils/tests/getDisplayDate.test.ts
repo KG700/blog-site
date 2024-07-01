0535b8d9647f88415e5582a5b104def4e4621ee3
Karla Gardiner <karla.gardiner@digital.cabinet-office.gov.uk>
2024-07-01 15:36:24 +0100

    4: added a passing test

---
+++ src/app/utils/tests/getDisplayDate.test.ts
@@ -0,0 +1,7 @@
+import { getDisplayDate } from '../getDisplayDate';
+
+describe('getDisplayDate', () => {
+    it('returns date with timestamp when passed isoDate and timeStamp true', () => {
+        expect('Hello').toEqual('Hello')
+    })
+})
