9e287780f7afdc28a0d97d7643b946d2d95bbbae
Karla Gardiner <karla.gardiner@digital.cabinet-office.gov.uk>
2024-07-02 11:52:40 +0100

    73: added unit tests for admin page component

--- jest.config.js
+++ jest.config.js
@@ -143,7 +143,7 @@ const config = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
@@ -152,7 +152,7 @@ const config = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  // testEnvironment: "jest-environment-node",
+  testEnvironment: "jest-environment-jsdom",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
