6339fe839395fe4fa8a4e3449f9e9bd630210aba
Karla Gardiner <karla.gardiner@digital.cabinet-office.gov.uk>
2024-07-02 12:19:17 +0100

    73: removed a mocked dependencies

--- src/app/admin/tests/pages.test.ts
+++ src/app/admin/tests/pages.test.ts
@@ -1,10 +1,9 @@
 import Admin from '../page';
 import { fireEvent, render, screen } from '@testing-library/react';
 import { Authenticator } from '@aws-amplify/ui-react';
-import config from '../../../aws-exports';
 import '@testing-library/jest-dom'
 
-jest.mock('../../../aws-exports');
+// jest.mock('../../../aws-exports');
 jest.mock('aws-amplify');
 jest.mock('@aws-amplify/ui-react', () => {
     return {
