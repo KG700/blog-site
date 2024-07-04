92ff506df937658bb90747da33d705f74766be26
Karla Gardiner <karla.gardiner@digital.cabinet-office.gov.uk>
2024-07-04 17:53:17 +0100

    83: added unit tests for summary component and fixed small bug

--- src/app/components/blog-summary.tsx
+++ src/app/components/blog-summary.tsx
@@ -1,29 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface Props {
-    value?: string
-    onChange: any
+  value?: string
+  onChange: any
 }
 
 export default function BlogSummary({ onChange, value }: Readonly<Props>) {
   const [count, setCount] = useState(0);
 
-    return (
-        <label className="block mt-4 mb-10">
-        <span className="text-sm font-normal">Summary</span>
-        <textarea
-          className="border-b pb-2 text-l focus:outline-none w-full text-gray-500 placeholder:text-gray-500 placeholder:font-normal placeholder:text-base y-2"
-          placeholder="Enter a blog summary"
-          name="summary"
-          value={value}
-          maxLength={1000}
-          rows={6}
-          onChange={(e) => {
-            onChange(e);
-            setCount(e?.target.value.length ?? 0);
-          }}
-        />
-        <p className="float-right">{count}/1000</p>
-      </label>
-    )
+  useEffect(() => {
+    setCount(value?.length ?? 0);
+  }, [])
+
+  return (
+    <label className="block mt-4 mb-10">
+    <span className="text-sm font-normal">Summary</span>
+    <textarea
+      className="border-b pb-2 text-l focus:outline-none w-full text-gray-500 placeholder:text-gray-500 placeholder:font-normal placeholder:text-base y-2"
+      placeholder="Enter a blog summary"
+      name="summary"
+      value={value}
+      maxLength={1000}
+      rows={6}
+      onChange={(e) => {
+        onChange(e);
+        setCount(e?.target.value.length ?? 0);
+      }}
+    />
+    <p className="float-right">{count}/1000</p>
+    </label>
+  )
 }
