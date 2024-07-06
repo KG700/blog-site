f5957f94839a0d586add7bab802e6bb05d8fd6a6
Karla Gardiner <karla.gardiner@digital.cabinet-office.gov.uk>
2024-07-06 15:14:59 +0100

    86: added unit tests to getDisplayDate

--- src/app/utils/tests/getDisplayDate.test.ts
+++ src/app/utils/tests/getDisplayDate.test.ts
@@ -10,9 +10,52 @@ describe('getDisplayDate', () => {
         jest.useRealTimers();
     });
 
-    it('returns today when passed todays isoDate', () => {
-        const todaysDate = new Date()
-        const theDate = getDisplayDate(todaysDate.toISOString())
-        expect(theDate).toEqual('today')
+    describe('when timestamp not required', () => {
+        it('returns today when passed todays isoDate', () => {
+            const todaysDate = new Date()
+            const theDate = getDisplayDate(todaysDate.toISOString())
+            expect(theDate).toEqual('today')
+        })
+
+        it('returns yesteday when passed yesterdays isoDate', () => {
+            const todaysDate = new Date()
+            const yesterdaysDate = new Date(todaysDate.setDate(todaysDate.getDate() - 1));
+
+            const theDate = getDisplayDate(yesterdaysDate.toISOString())
+            expect(theDate).toEqual('yesterday')
+        })
+
+        it('returns date when passed date from 2 days ago isoDate', () => {
+            const todaysDate = new Date()
+            const twoDaysAgoDate = new Date(todaysDate.setDate(todaysDate.getDate() - 2));
+
+            const theDate = getDisplayDate(twoDaysAgoDate.toISOString())
+            expect(theDate).toEqual('13 Jul 2024')
+        })
     })
+
+    describe('when timestamp is required', () => {
+        it('returns today when passed todays isoDate', () => {
+            const todaysDate = new Date()
+            const theDate = getDisplayDate(todaysDate.toISOString(), true)
+            expect(theDate).toEqual('today at 1:00')
+        })
+
+        it('returns yesteday when passed yesterdays isoDate', () => {
+            const todaysDate = new Date()
+            const yesterdaysDate = new Date(todaysDate.setDate(todaysDate.getDate() - 1));
+
+            const theDate = getDisplayDate(yesterdaysDate.toISOString(), true)
+            expect(theDate).toEqual('yesterday at 1:00')
+        })
+
+        it('returns date when passed date from 2 days ago isoDate', () => {
+            const todaysDate = new Date()
+            const twoDaysAgoDate = new Date(todaysDate.setDate(todaysDate.getDate() - 2));
+
+            const theDate = getDisplayDate(twoDaysAgoDate.toISOString(), true)
+            expect(theDate).toEqual('13 Jul 2024 at 1:00')
+        })
+    })
+
 })
